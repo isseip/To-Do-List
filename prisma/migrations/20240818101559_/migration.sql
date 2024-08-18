6163b9aced41d94c97b2276392908ea8831f19b3
isseip <prathmeshgolande0711@gmail.com>
2024-08-18 16:01:47 +0530

    changed the sqllite to psql

--- prisma/migrations/20240818101559_/migration.sql
+++ prisma/migrations/20240818101559_/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "finished" BOOLEAN NOT NULL
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
